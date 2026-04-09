25ab8ae5678670c0a87791ff6e7f998bb8afcc3f
Edward (Zehua) Zhang <edward.zehua.zhang@gmail.com>
2026-04-09 13:51:48 +0800

    feartf

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './globals.css';
 import App from './App.tsx';
 
+// iOS Safari doesn't always fire a resize event after orientation change,
+// so we dispatch one manually so R3F and any layout code recalculate correctly.
+window.addEventListener('orientationchange', () => {
+  setTimeout(() => window.dispatchEvent(new Event('resize')), 300);
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
